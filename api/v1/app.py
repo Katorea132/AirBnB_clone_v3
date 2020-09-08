e3f0a07a16c080e9c792fffa803995805962a4c1
Katorea132 <katorea132@gmail.com>
2020-09-08 16:15:19 -0500

    Adding all the way up to the error point

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """API module"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from api.v1.views import app_views
 from models import storage
 from os import getenv
@@ -15,6 +15,10 @@ def closedb(foo):
     """Tear and rip
     """
     storage.close()
+
+@app.errorhandler(404)
+def fourofour(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 if __name__ == '__main__':
     host = getenv("HBNB_API_HOST", "0.0.0.0")
